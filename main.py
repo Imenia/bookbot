7f226cd6a289cef96e80a993d2312b798a8d2874
Imenia <amon.hudon137@gmail.com>
2025-08-16 03:00:21 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def get_book_text(book):
         with open(book) as f:
             file_contents = f.read()
-        return file_contents
+        return file_contents     
 
 def main():
     book = "/home/test/bootdev_projects/bookbot/books/frankenstein.txt"
-    print(get_book_text(book))
+    text = (get_book_text(book))
+    num_words(text)
+    
+def num_words(text):
+    count = len(text.split())
+    print(f"{count} words found in the document")
+    
 
 main()
