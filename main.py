72f931c20793b4f7034edff2f1c9a34bdbc36a45
Imenia <amon.hudon137@gmail.com>
2025-08-16 01:21:30 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(book):
+        with open(book) as f:
+            file_contents = f.read()
+        return file_contents
+
+def main():
+    book = "/home/test/bootdev_projects/bookbot/books/frankenstein.txt"
+    print(get_book_text(book))
+
+main()
