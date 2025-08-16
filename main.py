500a2050c3fcd116279520ebf74415c14fb5f62b
Imenia <amon.hudon137@gmail.com>
2025-08-16 03:07:37 -0400

    update main.py; add stats.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import num_words
+
 def get_book_text(book):
         with open(book) as f:
             file_contents = f.read()
@@ -7,10 +9,5 @@ def main():
     book = "/home/test/bootdev_projects/bookbot/books/frankenstein.txt"
     text = (get_book_text(book))
     num_words(text)
-    
-def num_words(text):
-    count = len(text.split())
-    print(f"{count} words found in the document")
-    
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def num_words(text):
+    count = len(text.split())
+    print(f"{count} words found in the document")
